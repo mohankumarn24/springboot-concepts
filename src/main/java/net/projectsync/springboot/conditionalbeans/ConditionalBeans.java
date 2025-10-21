4bc44d654e790b7e89b0d85d00d39f3fbf99d674
mohankumarn24 <mohankumar.n24@outlook.com>
2025-10-21 20:41:36 +0530

    add TODO

--- src/main/java/net/projectsync/springboot/conditionalbeans/ConditionalBeans.java
+++ src/main/java/net/projectsync/springboot/conditionalbeans/ConditionalBeans.java
@@ -2,5 +2,8 @@ package net.projectsync.springboot.conditionalbeans;
 
 public class ConditionalBeans {
 
-	// TODO
+	// TODO: https://chatgpt.com/share/68f76bc4-0270-8004-918c-ad31a7d39f3d
+	public static void main(String[] args) {
+
+	}
 }
